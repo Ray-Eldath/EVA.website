fb868c4ccf13deb1b048975ce2ff643e022845b1
Ray-Eldath <ray.eldath@gmail.com>
2018-04-15 01:09:49 +0800

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="nav-bar">
+    <div class="content">
+      <div class="ui inverted segment inner">
+        <div class="ui inverted secondary pointing menu">
+          <div class="item icon">
+            <img src="../assets/logo.png" alt="Evangelion">
+          </div>
+          <a class="active item">Home</a>
+          <a class="item">Messages</a>
+          <a class="item">好友</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #nav-bar {
+    position: fixed;
+    width: 100%;
+    bottom: 60px;
+    overflow: hidden;
+    z-index: 10000;
+  }
+
+  #nav-bar .content {
+    height: 80px;
+    line-height: 100px;
+    width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: black;
+    border-radius: 40px;
+    display: table;
+  }
+
+  .inner {
+    background: transparent !important;
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .menu .icon {
+    padding: 0 !important;
+    margin-right: 20px !important;
+    margin-left: 16px !important;
+  }
+
+  .menu .icon img {
+    height: 40px;
+    width: auto !important;
+  }
+
+</style>
+
+<script>
+  export default {}
+
+</script>
---
+++ src/router/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <NavBar/>
+    <div id="fullpage">
+      <div class="section">
+        <h1>啦啦啦</h1>
+      </div>
+      <div class="section">Some section</div>
+      <div class="section">Some section</div>
+      <div class="section">Some section</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #fullpage {
+    margin-top: -60px;
+  }
+
+</style>
+<script>
+  import NavBar from '../components/NavBar'
+  export default {
+    components: {
+      NavBar
+    },
+    mounted: function () {
+      $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        menu: '#menu',
+        navigation: true,
+        continuousVertical: true,
+        scrollingSpeed: 1000
+      });
+    }
+  }
+
+</script>
